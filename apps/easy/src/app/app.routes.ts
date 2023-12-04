1146aa27c86b62f460e87e63c1951935a727ecb2
RemyPr97 <remyprince97@gmail.com>
2023-12-04 11:20:01 +0100

    ajout page login

--- apps/easy/src/app/app.routes.ts
+++ apps/easy/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Route } from '@angular/router';
-import {HomeComponent} from '@easy/home';
-import {ListComponent} from '@easy/list';
+import { HomeComponent } from '@easy/home';
+import { ListComponent } from '@easy/list';
+import { LoginComponent } from "@easy/login";
 
 
 export const appRoutes: Route[] = [
     { path: 'home',component: HomeComponent},
-    { path: 'list',component: ListComponent}
+    { path: 'list',component: ListComponent},
+    { path: 'login',component: LoginComponent},
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home' }
 ];
